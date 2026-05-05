4b68af3e5ab7d8cd52918077058ee10dd9ab14be
gabriaum <gaabrielbruck@gmail.com>
2026-05-05 16:16:30 -0300

    first commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,56 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+typedef struct {
+    char estado;
+    char codigo[8];
+    char nomeDaCidade[28];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+} Carta;
+
+void makeCarta(Carta *carta, int numero) {
+    printf("\n===== Registro da carta %i =====\n", numero);
+    printf("Estado:\n");
+    scanf(" %c", &carta->estado);
+
+    printf("Codigo:\n");
+    scanf("%s", carta->codigo);
+
+    printf("Nome:\n");
+    scanf("%s", carta->nomeDaCidade);
+
+    printf("Populacao:\n");
+    scanf("%d", &carta->populacao);
+
+    printf("Area:\n");
+    scanf("%f", &carta->area);
+
+    printf("PIB:\n");
+    scanf("%f", &carta->pib);
+
+    printf("Pontos turisticos:\n");
+    scanf("%d", &carta->pontosTuristicos);
+}
+
+void mostrarCarta(Carta *c, int numero) {
+    printf("\n=== Carta %d ===\n", numero);
+    printf("Estado: %c\n", c->estado);
+    printf("Código: %s\n", c->codigo);
+    printf("Nome da Cidade: %s\n", c->nomeDaCidade);
+    printf("População: %d\n", c->populacao);
+    printf("Área: %.2f km²\n", c->area);
+    printf("PIB: %.2f bilhões de reais\n", c->pib);
+    printf("Número de Pontos Turísticos: %d\n", c->pontosTuristicos);
+}
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    Carta carta1, carta2;
 
-  // Área para entrada de dados
+    makeCarta(&carta1, 1);
+    makeCarta(&carta2, 2);
 
-  // Área para exibição dos dados da cidade
-
-return 0;
-} 
+    mostrarCarta(&carta1, 1);
+    mostrarCarta(&carta2, 2);
+}
